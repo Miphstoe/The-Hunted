e9d84cf63b8ade5c0c3de8762be9ab8e1636b909
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-08 10:45:40 -0400

    Update city_manager.lua

--- MMOCoreORB/bin/scripts/managers/city_manager.lua
+++ MMOCoreORB/bin/scripts/managers/city_manager.lua
@@ -61,7 +61,7 @@ CityVotingCycles = 3
 CityVotingCyclesUntilLocked = 2
 
 --The amount of time in minutes before the city performs an update.
-CityUpdateInterval = 60 * 24
+CityUpdateInterval = 60 * 24 * 7
 
 --The amount of time in minutes a new city has to gain enough citizens to remain a city.
 NewCityGracePeriod = 60 * 24
