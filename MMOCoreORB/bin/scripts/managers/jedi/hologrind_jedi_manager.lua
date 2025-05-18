baf7b903e9bfdba57c77344f7d98e7d6ef160b6b
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-18 08:01:16 -0400

    Update hologrind_jedi_manager.lua
    Changed to 10 Professioins

--- MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
@@ -4,8 +4,8 @@ local PlayerManager = require("managers.player_manager")
 
 jediManagerName = "HologrindJediManager"
 
-NUMBEROFPROFESSIONSTOMASTER = 5 --this is now how many profs are selected
-MAXIMUMNUMBEROFPROFESSIONSTOSHOWWITHHOLOCRON = 6
+NUMBEROFPROFESSIONSTOMASTER = 10 --this is now how many profs are selected
+MAXIMUMNUMBEROFPROFESSIONSTOSHOWWITHHOLOCRON = 11
 
 HologrindJediManager = JediManager:new {
 	screenplayName = jediManagerName,
