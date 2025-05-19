e56484fc53a0c2f837148fb1a490909ab89dd133
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-18 21:50:04 -0400

    Update PlayerManagerImplementation.cpp

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -111,7 +111,7 @@
 #include "server/zone/objects/transaction/TransactionLog.h"
 #include "server/zone/objects/creature/commands/TransferItemMiscCommand.h"
 //#include "templates/crcstringtable/CrcStringTable.h"
-#include "server/zone/objects/ship/PobShipObject.h"
+//#include "server/zone/objects/ship/PobShipObject.h"
 #include "server/zone/objects/ship/ai/ShipAiAgent.h"
 #include "server/zone/objects/tangible/deed/ship/ShipDeed.h"
 #include "server/zone/packets/object/transform/Transform.h"
