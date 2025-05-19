5c03fd7769fc9b3a5d87f3125221065d3dbfd63b
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-19 10:16:54 -0400

    Update krayt_dragon_pearl.lua

--- MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_pearl.lua
+++ MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_pearl.lua
@@ -7,7 +7,7 @@ krayt_dragon_pearl = {
 	customObjectName = "A Krayt Dragon Pearl",
 	directObjectTemplate = "object/tangible/component/weapon/lightsaber/lightsaber_module_krayt_dragon_pearl.iff",
 	craftingValues = {
-		{"color",0,30,0},--actual color now managed in lootmanagerimp.cpp
+		--{"color",0,30,0},--actual color now managed in lootmanagerimp.cpp
 		{"mindamage", 20,150,0},
 		{"maxdamage", 40,225,0},
 		{"attackspeed",0.0,-0.6,1},
