39ab7697b8235d70f19e0569d3a218886a8be562
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-21 05:41:19 -0400

    Update GroupManager.cpp

--- MMOCoreORB/src/server/zone/managers/group/GroupManager.cpp
+++ MMOCoreORB/src/server/zone/managers/group/GroupManager.cpp
@@ -52,14 +52,14 @@ void GroupManager::inviteToGroup(CreatureObject* leader, CreatureObject* target)
 	if (leader->isGrouped()) {
 		ManagedReference<GroupObject*> group = leader->getGroup();
 
-		//if (playerIsInvitingOwnPet(leader, target)) {
-			//if (!target->isInRange(leader, 100000.0f)) {
-			//return;
-			//}
+		if (playerIsInvitingOwnPet(leader, target)) {
+			if (!target->isInRange(leader, 100000.0f)) {
+			return;
+		      }
 		}
 		else if (group->getLeader() != leader) {
-			leader->sendSystemMessage("@group:must_be_leader");
-			return;
+			//leader->sendSystemMessage("@group:must_be_leader");
+			//return;
 		}
 
 		// can't invite if the group is full
